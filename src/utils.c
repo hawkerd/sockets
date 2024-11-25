1018c361546ff0f6b97a33896090ebde06982f3d
hawkerd <dhawk064@gmail.com>
2024-11-25 10:26:01 -0600

    implement server utility functions init(), accept_connection(), send_file_to_client(), and get_request_server()

--- src/utils.c
+++ src/utils.c
@@ -27,16 +27,16 @@
 #include <stdint.h>
 
 //TODO: Declare a global variable to hold the file descriptor for the server socket
+int server_socket;
 
 //TODO: Declare a global variable to hold the mutex lock for the server socket
+pthread_mutex_t server_socket_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 //TODO: Declare a gloabl socket address struct to hold the address of the server
+struct sockaddr_in server_address;
 
-/*
-################################################
-##############Server Functions##################
-################################################
-*/
+
+// SERVER FUNCTIONS
 
 /**********************************************
  * init
@@ -46,36 +46,45 @@
    - if init encounters any errors, it will call exit().
 ************************************************/
 void init(int port) {
-   //TODO: create an int to hold the socket file descriptor
-   //TODO: create a sockaddr_in struct to hold the address of the server
-
-
-   /**********************************************
-    * IMPORTANT!
-    * ALL TODOS FOR THIS FUNCTION MUST BE COMPLETED FOR THE INTERIM SUBMISSION!!!!
-    **********************************************/
+  int socket_fd;
+  //struct sockaddr_in address;
    
-   
-   
-   // TODO: Create a socket and save the file descriptor to sd (declared above)
-   
-   // TODO: Change the socket options to be reusable using setsockopt(). 
-
-
-   // TODO: Bind the socket to the provided port.
+  // Create a socket
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    perror("System call socket() failed");
+    exit(EXIT_FAILURE);
+  }
   
+  // Set socket options
+  int enable = 1;
+  if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+    perror("System call setsockopt() failed");
+    exit(EXIT_FAILURE);
+  }
 
-   // TODO: Mark the socket as a pasive socket. (ie: a socket that will be used to receive connections)
+  // Initialize the address struct
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  server_address.sin_port = htons(port);
 
-   
-   
-   
-   // We save the file descriptor to a global variable so that we can use it in accept_connection().
-   // TODO: Save the file descriptor to the global variable master_fd
+  // Bind the socket
+  if (bind(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+      perror("System call bind() failed");
+      exit(EXIT_FAILURE);
+  }
 
-   printf("UTILS.O: Server Started on Port %d\n",port);
-   fflush(stdout);
+  // Mark the socket as a pasive socket
+  if (listen(socket_fd, SOMAXCONN) < 0) {
+      perror("System call listen() failed");
+      exit(EXIT_FAILURE);
+  }
+  
+  // We save the file descriptor to a global variable so that we can use it in accept_connection().
+  server_socket = socket_fd;
 
+  printf("UTILS.O: Server Started on Port %d\n",port);
+  fflush(stdout);
 }
 
 
@@ -86,26 +95,31 @@ void init(int port) {
      accept_connection must should ignore request.
 ***********************************************/
 int accept_connection(void) {
-
-   //TODO: create a sockaddr_in struct to hold the address of the new connection
+  struct sockaddr_in new_addr;
+  socklen_t addr_len = sizeof(new_addr);
+  int newsock;
   
-   
-   /**********************************************
-    * IMPORTANT!
-    * ALL TODOS FOR THIS FUNCTION MUST BE COMPLETED FOR THE INTERIM SUBMISSION!!!!
-    **********************************************/
-   
-   
-   
-   // TODO: Aquire the mutex lock
+  // Acquire the mutex lock
+  if (pthread_mutex_lock(&server_socket_mtx) < 0) {
+    perror("System call lock() failed");
+    return -1;
+  }
 
-   // TODO: Accept a new connection on the passive socket and save the fd to newsock
+  // Accept a new connection on the passive socket
+  newsock = accept(server_socket, (struct sockaddr*)&new_addr, &addr_len);
+  if (newsock < 0) {
+    pthread_mutex_unlock(&server_socket_mtx);
+    perror("System call accept() failed");
+    return -1;
+  }
+  
+  // Release the mutex lock
+  if (pthread_mutex_unlock(&server_socket_mtx) < 0) {
+    perror("System call unlock() failed");
+    return -1;
+  }
 
-   // TODO: Release the mutex lock
-
-
-   // TODO: Return the file descriptor for the new client connection
-   
+  return newsock;
 }
 
 
@@ -116,18 +130,28 @@ int accept_connection(void) {
    - size is the size of the file you want to send
    - returns 0 on success, -1 on failure 
 ************************************************/
-int send_file_to_client(int socket, char * buffer, int size) 
-{
-    //TODO: create a packet_t to hold the packet data
- 
+int send_file_to_client(int socket, char * buffer, int size) {
+  // Send the file size packet
+  packet_t packet;
+  packet.size = size;
 
-    //TODO: send the file size packet
+  if (send(socket, &packet, sizeof(packet), 0) < 0) {
+    perror("System call send() failed");
+    return -1;
+  }
 
+  // Send the file data
+  int bytes_sent = 0;
+  while (bytes_sent < size) {
+    int sent_bytes = send(socket, buffer + bytes_sent, (size_t)size - bytes_sent, 0);
+    if (sent_bytes < 0) {
+      perror("System call send() failed");
+      return -1;
+    }
+    bytes_sent += sent_bytes;
+  }
 
-    //TODO: send the file data
-  
-    //TODO: return 0 on success, -1 on failure
-
+  return 0;
 }
 
 
@@ -139,16 +163,40 @@ int send_file_to_client(int socket, char * buffer, int size)
 ************************************************/
 char * get_request_server(int fd, size_t *filelength)
 {
-    //TODO: create a packet_t to hold the packet data
- 
-    //TODO: receive the response packet
+  // Receive the size packet
+  packet_t packet;
+  if (recv(fd, &packet, sizeof(packet), 0) < 0) {
+    perror("System call recv() failed");
+    return NULL;
+  }
   
-    //TODO: get the size of the image from the packet
+  // Get the size of the image
+  *filelength = packet.size;
+  if (packet.size == 0) {
+    printf("Received empty image");
+    return NULL;
+  }
 
-    //TODO: recieve the file data and save into a buffer variable.
+  // Allocate memory
+  char* buffer = malloc(*filelength);
+  if (buffer == NULL) {
+    perror("System call malloc() failed");
+    return NULL;
+  }
 
-    //TODO: return the buffer
+  // Receive data
+  size_t bytes_received = 0;
+  while (bytes_received < packet.size) {
+    int received_bytes = recv(fd, buffer + bytes_received, packet.size - bytes_received, 0);
+    if (received_bytes < 0) {
+      perror("System call perror() failed");
+      free(buffer);
+      return NULL;
+    }
+    bytes_received += received_bytes;
+  }
 
+  return buffer;
 }
 
 
